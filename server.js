1ea8e01d810da1e9884ca6c30110f92a9c763731
Abyss0150 <adityanegi281@gmail.com>
2025-01-04 17:18:24 +0530

    setting up monngodb

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const {connectMongoDB} = require("./connection");
+
 const app = express();
+
+dotenv.config();
+const PORT = process.env.PORT;
+const URI = process.env.URI;
+
