1a3ba21037e8a57e04ed6e898916eb0536d0bf75
Abyss0150 <adityanegi281@gmail.com>
2025-01-04 18:58:06 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -5,9 +5,12 @@ const {connectMongoDB} = require("./connection");
 const app = express();
 
 dotenv.config();
-const PORT = process.env.PORT;
-const URI = process.env.URI;
+const PORT = process.env.PORT
+const URI = process.env.URI
 
-connectMongoDB(URI).then(()=> console.log("database connected"));
+connectMongoDB(URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+}).then(()=> console.log("database connected")).catch((err) => console.error('Error connecting to MongoDB:', err));
 
 app.listen(PORT, (req, res) => console.log(`Running at port ${PORT}`));
